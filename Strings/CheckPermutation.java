e43b845f9b02a8b345e8cc379f70b974d0a3448e
Naman Bakrewal <85246584+Naman082@users.noreply.github.com>
2023-01-02 18:41:24 +0530

    Check Permutation

--- Strings/CheckPermutation.java
+++ Strings/CheckPermutation.java
@@ -1,37 +1,38 @@
 /*
 String -> Check Permutation of the given String "str" 
 
-Sample Input 1:
-naman
-Sample Output 2:
+SAMPLE INPUT 1:
+abcde
+dcbea
+SAMPLE OUTPUT 2:
 true
 
-Sample Input 1:
-bakrewal
-Sample Output 2:
+SAMPLE INPUT 1:
+blgh
+ghbt
+SAMPLE OUTPUT 2:
 false
 
 */
 
 import java.util.Scanner;
-
 public class CheckPermutation {
 	
-	public static boolean isPermutation(String str1, String str2) {
+	private static boolean isPermutation(String str1, String str2) {
 		if(str1.length() != str2.length()) {
 			return false;
 		}
-		int[] frequency = new int[256];// length of ASCII 
-		for(int i=0;i<256;i++) {
+		int[] frequency = new int[256]; // length of ASCII 
+		for(int i=0;i<256;i++) { // Not strictly necessary as frequency array is of type integer so the default values are 0's
 			frequency[i] = 0;
 		}
 		for(int i=0;i<str1.length();i++) {
 			char ch = str1.charAt(i);
-			++frequency[ch];
+			++frequency[ch]; // frequency[ch]++; 
 		}
 		for(int i=0;i<str2.length();i++) {
 			char ch = str2.charAt(i);
-			--frequency[ch];
+			--frequency[ch]; // frequency[ch]--;
 		}
 		for(int i=0;i<256;i++) {
 			if(frequency[i] != 0)
@@ -46,5 +47,4 @@ public class CheckPermutation {
 		String str2 = s.nextLine();
 		System.out.print(isPermutation(str1, str2));
 	}
-
 }
